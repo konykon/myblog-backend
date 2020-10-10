f42048ffaa2ac2d7456ae3cff08671b7b98cdda8
konykon <tsagarikon@hotmail.com>
2020-10-10 18:28:39 +0200

    first commit

---
+++ src/build/precache-manifest.b61907563540cdaf09b5cfece0a37dfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f913905b0eb547c458dbc3b3132e169",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d79780dc163611950b48",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "89099ac2e46092ad0fc9",
+    "url": "/static/js/2.b4ee2042.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b4ee2042.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d79780dc163611950b48",
+    "url": "/static/js/main.ccf2c145.chunk.js"
+  },
+  {
+    "revision": "770344394e9f4453d0a9",
+    "url": "/static/js/runtime-main.3a907230.js"
+  }
+]);
